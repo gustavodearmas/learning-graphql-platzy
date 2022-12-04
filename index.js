6deec283ec44a96d1136b2569bc86d263d1c19d2
gustavodearmas <gustavo.dearmas1@gmail.com>
2022-12-03 22:19:05 -0500

    class number 5

---
+++ index.js
@@ -0,0 +1,41 @@
+"use strict";
+import { buildSchema } from "graphql";
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import  fs  from 'fs';
+import path, { join }  from "path";
+import { fileURLToPath } from 'url';
+import resolvers from "./lib/resolvers.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const PORT = process.env.port || 3000;
+
+// Definiendo el esquema
+const schema = buildSchema(
+    fs.readFileSync(join(__dirname, 'lib', 'schema.graphql'), 'utf-8')
+);
+
+app.use(
+    "/graphql",
+    graphqlHTTP({
+        schema: schema,
+        rootValue: resolvers,
+        graphiql: true,
+    })
+);
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}/graphql`);
+});
+
+// Este el e codigo para usar en la terminal - Importar { graphql } desde graphql
+// const rootValue = resolvers;
+// const source = '{ un_revolver_por_cada_query }';
+// graphql({ schema, source, rootValue }).then((response) => {
+//   console.log(response);
+// });
+
+//CORRECIÓN DE ERRORES
+// instarlar npm i standard -D
+// correr el comando npm run lint-fix
